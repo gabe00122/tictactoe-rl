03f173ba6217a78eca9f6680af1e97bb4dd38916
Gabriel keith <gabe.keith@yahoo.com>
2024-03-10 01:18:23 -0500

    minimax player

--- tictactoe_ai/minmax/minmax_player.py
+++ tictactoe_ai/minmax/minmax_player.py
@@ -1,54 +1,20 @@
 import jax
+from jax import numpy as jnp, random
 
 from tictactoe_ai.gamerules.types import GameState
-from tictactoe_ai.gamerules.turn import turn
-from tictactoe_ai.gamerules.initialize import initialize_game
-from typing import NamedTuple
-from jax import numpy as jnp
 
 
-turn = jax.jit(turn)
-counter = 0
+def get_action(optimal_play, game: GameState, rng_key):
+    board = (game["board"] + 1).flatten()
+    action_values = optimal_play[*board]
+    jax.debug.print("{}", action_values)
 
+    action_mask = board == 1
+    action_values *= -game["active_player"]
+    action_values = jnp.where(action_mask, action_values, jnp.inf)
 
-class Results(NamedTuple):
-    o_wins: int
-    ties: int
-    x_wins: int
+    best_actions = action_values == action_values.min()
+    count = jnp.count_nonzero(best_actions)
+    probs = best_actions / count
 
-
-def minmax(state: GameState) -> Results:
-    global counter
-    counter += 1
-    if counter % 1000 == 0:
-        print(counter)
-
-    if state['over_result'] != 0:
-        return Results(
-            int(state['over_result'] == 1),
-            int(state['over_result'] == 2),
-            int(state['over_result'] == 3),
-        )
-    else:
-        board = state['board'].flatten().tolist()
-        o_wins = 0
-        ties = 0
-        x_wins = 0
-
-        for action, cell in enumerate(board):
-            if cell == 0:
-                result = minmax(turn(state, jnp.int8(action)))
-                o_wins += result.o_wins
-                ties += result.ties
-                x_wins += result.x_wins
-
-        return Results(o_wins, ties, x_wins)
-
-
-def main():
-    results = minmax(initialize_game())
-    print(results)
-
-
-if __name__ == '__main__':
-    main()
+    return random.choice(rng_key, jnp.arange(9), p=probs)
